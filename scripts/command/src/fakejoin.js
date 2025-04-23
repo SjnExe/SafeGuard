d8ffcb4963eddcb6d485e68052cd3a4efbe10a3f
SjnExe <sjnexecontact@gmail.com>
2025-04-24 04:22:57 +0530

    Update and rename fakeleave.js to fakejoin.js

---
+++ scripts/command/src/fakejoin.js
@@ -0,0 +1,15 @@
+import { newCommand } from '../handle';
+import { sendMessageToAllAdmins } from "../../assets/util";
+import { world } from '@minecraft/server';
+
+
+newCommand({
+    name: "fakejoin",
+    description: "Simulate joining the server",
+    run: (data) => {
+        const { player } = data;
+
+        world.sendMessage(`§e${player.name} joined the game`);
+        sendMessageToAllAdmins(`§6[§eSafeGuard Notify§6]§e ${player.name}§f fake joined.`,true);
+    }
+})
